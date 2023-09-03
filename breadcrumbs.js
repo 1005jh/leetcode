db8ba8c5e81724d02b30778a850109589e071cab
1005jh <kjhhy1005@gmail.com>
2023-09-03 17:08:46 +0900

    breadcrumbs 최종

--- breadcrumbs.js
+++ breadcrumbs.js
@@ -1,75 +1,109 @@
-const mysql = require("mysql2");
-const express = require("express");
-const app = express();
-const port = 3000;
+// const mysql = require("mysql2");
+// const express = require("express");
+// const app = express();
+// const port = 3000;
 
-const connection = mysql.createConnection({
-  host: "127.0.0.1",
-  user: "root",
-  password: "1234",
-  database: "breadcrumbs",
-});
+// const connection = mysql.createConnection({
+//   host: "127.0.0.1",
+//   user: "root",
+//   password: "1234",
+//   database: "breadcrumbs",
+// });
 
-connection.connect();
+// connection.connect();
 
-app.get("/page/:id", (req, res) => {
-  const pageId = parseInt(req.params.id);
+// app.get("/page/:id", (req, res) => {
+//   try {
+//     const pageId = parseInt(req.params.id);
 
-  const query = `
-      SELECT 
-        p1.id AS pageId, p1.title AS title, p1.content AS content, p1.path AS pagePath,
-        p2.id AS subPageId, p2.title AS subPageTitle
-      FROM 
-        pages p1
-      LEFT JOIN 
-        pages p2 ON p1.id = p2.parentId
-      WHERE 
-        p1.id = ?
-    `;
+//     const query = `
+//             SELECT
+//               p1.id AS id, p1.title AS title, p1.content AS content, p1.path AS pagePath,
+//               p2.id AS subPageId, p2.title AS subPageTitle
+//             FROM
+//               pages p1
+//             LEFT JOIN
+//               pages p2 ON p1.id = p2.parentId
+//             WHERE
+//               p1.id = ?
+//           `;
 
-  connection.query(query, [pageId], (error, results) => {
-    if (error) {
-      return res.status(400).json({ error: error.message });
-    }
+//     connection.query(query, [pageId], (error, results) => {
+//       if (error) {
+//         return res.status(400).json({ error: error.message });
+//       }
 
-    if (results.length === 0) {
-      return res.status(404).json({ error: "Page not found" });
-    }
+//       if (results.length === 0) {
+//         return res.status(404).json({ error: "Page not found" });
+//       }
+//       console.log(results, "results");
+//       /**[
+//   {
+//     id: 4,
+//     title: '부모',
+//     content: '모',
+//     pagePath: [],
+//     subPageId: 7,
+//     subPageTitle: '자식1'
+//   },
+//   {
+//     id: 4,
+//     title: '부모',
+//     content: '모',
+//     pagePath: [],
+//     subPageId: 13,
+//     subPageTitle: '자식11'
+//   }
+// ]  results 의 값*/
+//       const { id, title, content, pagePath } = results[0];
+//       const subPages = results
+//         .map((item) => ({
+//           subPageId: item.subPageId,
+//           title: item.subPageTitle,
+//         }))
+//         .filter((subPage) => subPage.subPageId !== null);
 
-    const { pageId, title, content, pagePath } = results[0];
-    const subPages = results
-      .map((item) => ({
-        subPageId: item.subPageId,
-        title: item.subPageTitle,
-      }))
-      .filter((subPage) => subPage.subPageId !== null);
+//       const pathQuery = `SELECT title FROM pages WHERE id IN (?)`;
+//       // console.log(typeof pagePath, "ㅁㄴㅇㄹ");
+//       // console.log(pagePath, "ㅁㄴㅇㄹ");
+//       if (pagePath.length === 0) {
+//         const response = {
+//           pageId: id,
+//           title: title,
+//           content: content,
+//           subPages,
+//           breadcrumbs: [],
+//         };
+//         return res.status(200).json(response);
+//       }
 
-    const pathQuery = `SELECT title FROM pages WHERE id IN (?)`;
-    // console.log(typeof pagePath, "ㅁㄴㅇㄹ");
-    // console.log(pagePath, "ㅁㄴㅇㄹ");
+//       connection.query(pathQuery, [pagePath], (error, pathResults) => {
+//         if (error) {
+//           return res.status(400).json({ error: error.message });
+//         }
+//         console.log(pathResults, "pathResults");
+//         /**[ { title: '부모' }, { title: '자식1' }, { title: '자식2' } ] pathResults의 값 */
+//         const breadcrumbs = pathResults.map((item) => item.title);
 
-    connection.query(pathQuery, [pagePath], (error, pathResults) => {
-      if (error) {
-        return res.status(400).json({ error: error.message });
-      }
+//         const response = {
+//           pageId: id,
+//           title: title,
+//           content: content,
+//           subPages,
+//           breadcrumbs,
+//         };
 
-      const breadcrumbs = pathResults.map((item) => item.title);
-
-      const response = {
-        pageId,
-        title: title,
-        content: content,
-        subPages,
-        breadcrumbs,
-      };
-
-      res.status(200).json(response);
-    });
-  });
-});
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
-});
+//         res.status(200).json(response);
+//       });
+//     });
+//   } catch (error) {
+//     console.log(error, "error log");
+//     res.status(500).json({ error: "Internal server error" });
+//   }
+// });
+// app.listen(port, () => {
+//   console.log(`Server running at http://localhost:${port}/`);
+// });
 
 /**
  * mysql 로컬 db 접속 후 아래 순서대로 진행
@@ -101,3 +135,157 @@ INSERT INTO pages (title, content, parentId, path) VALUES ('자식2', '자식2',
 INSERT INTO pages (title, content, parentId, path) VALUES ('자식3', '자식3', 3, '[1,2,3]');
 
  */
+const mysql = require("mysql2");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+let connection;
+
+async function initialize() {
+  connection = await mysql.createConnection({
+    host: "127.0.0.1",
+    user: "root",
+    password: "1234",
+    database: "breadcrumbs",
+  });
+
+  app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+  });
+}
+
+initialize();
+
+app.get("/page/:id", async (req, res) => {
+  try {
+    const pageId = parseInt(req.params.id);
+
+    const query = `
+      SELECT 
+        p1.id AS id, p1.title AS title, p1.content AS content, p1.path AS pagePath,
+        p2.id AS subPageId, p2.title AS subPageTitle
+      FROM 
+        pages p1
+      LEFT JOIN 
+        pages p2 ON p1.id = p2.parentId
+      WHERE 
+        p1.id = ?
+    `;
+
+    const [results] = await connection.promise().query(query, [pageId]);
+
+    if (results.length === 0) {
+      return res.status(404).json({ error: "Page not found" });
+    }
+
+    const { id, title, content, pagePath } = results[0];
+
+    const subPages = results
+      .map((item) => ({
+        subPageId: item.subPageId,
+        title: item.subPageTitle,
+      }))
+      .filter((subPage) => subPage.subPageId !== null);
+
+    if (pagePath && pagePath.length === 0) {
+      const response = {
+        pageId: id,
+        title,
+        content,
+        subPages,
+        breadcrumbs: [],
+      };
+      return res.status(200).json(response);
+    }
+
+    const pathQuery = `SELECT title FROM pages WHERE id IN (?)`;
+    const [pathResults] = await connection
+      .promise()
+      .query(pathQuery, [pagePath]);
+
+    const breadcrumbs = pathResults.map((item) => item.title);
+
+    const response = {
+      pageId: id,
+      title,
+      content,
+      subPages,
+      breadcrumbs,
+    };
+
+    res.status(200).json(response);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.put("/page/:id", async (req, res) => {
+  try {
+    const { parentId } = req.body;
+    const pageId = parseInt(req.params.id);
+    const [currentPageResults] = await connection
+      .promise()
+      .query(`SELECT * FROM pages WHERE id = ?`, [pageId]);
+
+    if (currentPageResults.length === 0) {
+      return res.status(404).json({ error: "Page not found" });
+    }
+
+    const currentParentId = currentPageResults[0].parentId;
+    const currentPath = currentPageResults[0].path;
+
+    if (currentParentId === parentId) {
+      return res.status(400).json({
+        error: "parentId not same currentParentId",
+      });
+    }
+
+    let newPath = [];
+    if (parentId !== null) {
+      const [newParentResults] = await connection
+        .promise()
+        .query(`SELECT * FROM pages WHERE id = ?`, [parentId]);
+
+      if (newParentResults.length === 0) {
+        return res.status(404).json({ error: "New parent not found" });
+      }
+      newPath = [...newParentResults[0].path, parentId];
+    }
+
+    await connection
+      .promise()
+      .query(`UPDATE pages SET parentId = ?, path = ? WHERE id = ?`, [
+        parentId,
+        JSON.stringify(newPath),
+        pageId,
+      ]);
+
+    const updateDescendantsPath = async (parentId, parentPath) => {
+      const [children] = await connection
+        .promise()
+        .query(`SELECT * FROM pages WHERE parentId = ?`, [parentId]);
+
+      for (const child of children) {
+        const newChildPath = [...parentPath, child.id];
+        await connection
+          .promise()
+          .query(`UPDATE pages SET path = ? WHERE id = ?`, [
+            JSON.stringify(newChildPath),
+            child.id,
+          ]);
+        await updateDescendantsPath(child.id, newChildPath);
+      }
+    };
+
+    await updateDescendantsPath(pageId, newPath);
+
+    res.status(200).json({ message: "success" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
